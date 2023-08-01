0137c924fddd6c21d256f82d4e23649ca7a890b6
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-08-01 16:55:03 -0400

    Version 1.0 Beginner working function of app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const choices = ['rock', 'paper', 'scissors'];
+const roundsToWin = 2;
+
+const RockPaperScissors = () => {
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const [playerSelection, setPlayerSelection] = useState('');
+  const [computerSelection, setComputerSelection] = useState('');
+  const [result, setResult] = useState('');
+
+  const computerPlay = () => {
+    return choices[Math.floor(Math.random() * choices.length)];
+  };
+
+  const play = (playerSelection) => {
+    if (playerScore < roundsToWin && computerScore < roundsToWin) {
+      const computerSelection = computerPlay();
+      setPlayerSelection(playerSelection);
+      setComputerSelection(computerSelection);
+      const roundResult = determineWinner(playerSelection, computerSelection);
+      updateScore(roundResult);
+      setResult(roundResult);
+    }
+  };
+
+  const determineWinner = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+      return "It's a tie!";
+    } else if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'paper' && computerSelection === 'rock') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+      setPlayerScore((prevScore) => prevScore + 1);
+      return "You win!";
+    } else {
+      setComputerScore((prevScore) => prevScore + 1);
+      return "Computer wins!";
+    }
+  };
+
+  const updateScore = (roundResult) => {
+    if (playerScore >= roundsToWin || computerScore >= roundsToWin) {
+      announceWinner();
+    }
+  };
+
+  const announceWinner = () => {
+    if (playerScore > computerScore) {
+      setResult("Congratulations! You win the game!");
+    } else {
+      setResult("Game over. Computer wins!");
+    }
+  };
+
+  const resetGame = () => {
+    setPlayerScore(0);
+    setComputerScore(0);
+    setPlayerSelection('');
+    setComputerSelection('');
+    setResult('');
+  };
+
+  const disableButtons = () => {
+    return playerScore >= roundsToWin || computerScore >= roundsToWin;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Rock, Paper, Scissors</h1>
+      <p>Make your choice: 
+        <button onClick={() => play('rock')} disabled={disableButtons()}>Rock</button>
+        <button onClick={() => play('paper')} disabled={disableButtons()}>Paper</button>
+        <button onClick={() => play('scissors')} disabled={disableButtons()}>Scissors</button>
+      </p>
+      <p>You chose {playerSelection}, Computer chose {computerSelection}. {result}</p>
+      <p>Player: {playerScore} | Computer: {computerScore}</p>
+      {playerScore >= roundsToWin || computerScore >= roundsToWin ? (
+        <button onClick={resetGame}>Play Again</button>
+      ) : null}
     </div>
   );
-}
+};
 
-export default App;
+export default RockPaperScissors;
